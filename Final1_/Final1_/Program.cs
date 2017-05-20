217d6cdf288dfe6559902891fe5b059f5865bdc6
Alisher Baigazin <alisherbaigazin@gmail.com>
2017-05-20 09:38:55 +0600

    20

--- Final1_/Final1_/Program.cs
+++ Final1_/Final1_/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Final1_
 {
@@ -10,6 +11,19 @@ namespace Final1_
     {
         static void Main(string[] args)
         {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\1\Desktop\final_new\Final1_\Final1_\bin\Debug");
+            FileSystemInfo[] fl = dir.GetFileSystemInfos();
+            foreach(FileSystemInfo f in fl)
+            {
+                Console.WriteLine(f.Name);
+                Console.ReadKey();
+            }
+            StreamReader file1 = new StreamReader(@"C:\Users\1\Desktop\final_new\Final1_\Final1_\bin\Debug\file1.txt");
+            StreamReader file2 = new StreamReader(@"C:\Users\1\Desktop\final_new\Final1_\Final1_\bin\Debug\file2.txt");
+            StreamReader file3 = new StreamReader(@"C:\Users\1\Desktop\final_new\Final1_\Final1_\bin\Debug\file3.txt");
+
+            
+
         }
     }
 }
