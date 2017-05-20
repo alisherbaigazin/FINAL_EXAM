21d0b4c5bf54b392c5fb8b189f3a40a27029b64a
Alisher Baigazin <alisherbaigazin@gmail.com>
2017-05-20 10:11:33 +0600

    10:10

--- Final2/Final2/Form1.Designer.cs
+++ Final2/Final2/Form1.Designer.cs
@@ -55,6 +55,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "0";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.TextChanged += new System.EventHandler(this.button_Click);
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
@@ -65,6 +66,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "0";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.TextChanged += new System.EventHandler(this.button_Click);
             // 
             // button3
             // 
@@ -74,6 +76,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "0";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.TextChanged += new System.EventHandler(this.button_Click);
             // 
             // button4
             // 
@@ -83,6 +86,7 @@
             this.button4.TabIndex = 4;
             this.button4.Text = "0";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.TextChanged += new System.EventHandler(this.button_Click);
             // 
             // button5
             // 
@@ -92,6 +96,7 @@
             this.button5.TabIndex = 5;
             this.button5.Text = "0";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.TextChanged += new System.EventHandler(this.button_Click);
             // 
             // button6
             // 
@@ -101,6 +106,7 @@
             this.button6.TabIndex = 6;
             this.button6.Text = "0";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.TextChanged += new System.EventHandler(this.button_Click);
             // 
             // button7
             // 
@@ -110,6 +116,7 @@
             this.button7.TabIndex = 7;
             this.button7.Text = "0";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.TextChanged += new System.EventHandler(this.button_Click);
             // 
             // button8
             // 
@@ -119,6 +126,7 @@
             this.button8.TabIndex = 8;
             this.button8.Text = "0";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.TextChanged += new System.EventHandler(this.button_Click);
             // 
             // button9
             // 
@@ -128,6 +136,7 @@
             this.button9.TabIndex = 9;
             this.button9.Text = "0";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.TextChanged += new System.EventHandler(this.button_Click);
             // 
             // Form1
             // 
--- Final2/Final2/Form1.cs
+++ Final2/Final2/Form1.cs
@@ -12,7 +12,8 @@ namespace Final2
 {
     public partial class Form1 : Form
     {
-        
+        int a = 1;
+        int t = 0;
         public Form1()
         {
             InitializeComponent();
@@ -20,13 +21,24 @@ namespace Final2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            for(int i = 0; i < 1000;i++)
+          
+            
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            int n = a++;
+            Button button = sender as Button;
+            button.Text = n.ToString();
+            textBox1.Text = n.ToString();
+            if (n % 2!=0)
             {
-                int n = i;
+                t++;
                 
             }
-            button1.Text = n;
-            
+            textBox1.Text = t.ToString();
+
+
         }
     }
 }
